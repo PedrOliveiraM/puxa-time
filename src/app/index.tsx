4a5fb8f6520aa51ea977dab4de200218c2497233
PedrOliveiraM <pedropucmont@gmail.com>
2025-01-26 19:26:17 -0300

    feat: add babel configuration and implement animated logo in index component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,32 @@ import { Button } from '@/components/button'
 import { s } from '@/styles/styles.index'
 import { LinearGradient } from 'expo-linear-gradient'
 import { router } from 'expo-router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated'
 
 export default function Index() {
+  const scale = useSharedValue(1)
+
+  // Configura a animação de escala
+  useEffect(() => {
+    scale.value = withRepeat(
+      withTiming(1.1, { duration: 1000 }),
+      -1, // Repetir para sempre
+      true // Inverte a animação (vai e volta)
+    )
+  }, [])
+
+  // Estilo animado
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }))
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <LinearGradient
@@ -13,7 +35,12 @@ export default function Index() {
         style={StyleSheet.absoluteFillObject}
       />
       <View style={[s.container]}>
-        <Image source={require('@/assets/Logo.png')} style={s.logo} />
+        {/* Logo com animação */}
+        <Animated.View style={animatedStyle}>
+          <Image source={require('@/assets/Logo.png')} style={s.logo} />
+        </Animated.View>
+
+        {/* Textos e botão */}
         <Text style={s.title}>Boas vindas ao Puxa Time!</Text>
         <Text style={s.subTitle}>
           Uma nova forma de escolher e organizar seu futebol.
